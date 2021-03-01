3e5d40043eff72c51862f2ddae18d16832599b5e
mario-gallegobecerra-plx <mario.gallegobecerra@plexus.es>
2021-03-01 15:26:58 +0100

    login app with facebook

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
